1d6234ebe08ea8c5c1af2407cd1fafbe2c7f34f4
jaideepteo <jaideepsingh94566@gmail.com>
2026-06-02 11:43:52 +0530

    new-image-changes

--- blocks/new-image/new-image.js
+++ blocks/new-image/new-image.js
@@ -1,7 +1,3 @@
-import { getMetadata } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-      const ogTitle = getMetadata('og:title');
-      const ogDesc = getMetadata('og:description'); 
-      console.log(ogDesc);
 }
